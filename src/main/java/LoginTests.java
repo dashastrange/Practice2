7f01ef774ac2646961d3eba214f93fe00d46b5a5
dashastrange <dashastrange@gmail.com>
2016-11-25 16:43:04 +0200

    Assert Player info test added

--- src/main/java/LoginTests.java
+++ src/main/java/LoginTests.java
@@ -14,8 +14,7 @@ public class LoginTests {
         String URL = "http://80.92.229.236:81"; // Poker URL
         driver.get(URL + "/auth/login"); // Open Poker
         String loginName = generateRandomString(5); //generate random player name
-        String actualTitle = driver.getTitle();
-        String expectedTitle = "Players";
+        String email = loginName + "@gmal.com";
 
         WebElement usernameInput = driver.findElement(By.id("username")); // Find username input
         usernameInput.sendKeys("admin"); // Set username
@@ -26,8 +25,6 @@ public class LoginTests {
         WebElement loginButton = driver.findElement(By.id("logIn")); // Find login button
         loginButton.click(); // click on LogIn button
 
-        assertText(actualTitle, expectedTitle); // Make assertions
-
 //TODO Insert player part
 
         WebElement insertButton = driver.findElement(By.linkText("Insert"));
@@ -37,7 +34,7 @@ public class LoginTests {
         loginInput.sendKeys(loginName);
 
         WebElement emailInput = driver.findElement(By.xpath("//*[contains(@name, 'email')]"));
-        emailInput.sendKeys(loginName + "@gmail.com");
+        emailInput.sendKeys(email);
 
         WebElement passInput = driver.findElement(By.xpath("//*[contains(@name, 'password')]"));
         passInput.sendKeys("111111");
@@ -60,9 +57,48 @@ public class LoginTests {
         WebElement saveButton = driver.findElement(By.name("button_save"));
         saveButton.click();
 
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Players";
+
         assertText(actualTitle, expectedTitle);
-        driver.close();
-        driver.quit(); // Close Browser
+
+        WebElement searchInput = driver.findElement(By.xpath("//*[contains(@name, 'login') and not(@class)]"));
+        searchInput.sendKeys(loginName);
+
+        WebElement searchButton = driver.findElement(By.name("search"));
+        searchButton.click();
+
+        WebElement editButton = driver.findElement(By.xpath("//tr[last()]/td[1]/a"));
+        //.//tr[.//a[text()=' " + loginName + " ']]//img[@title='Edit']
+        editButton.click();
+
+        WebElement emailField = driver.findElement(By.xpath("//*[contains(@name, 'email')]"));
+        String actualValue = emailField.getAttribute("value");
+        String expectedValue = email;
+        assertValue(actualValue, expectedValue);
+
+        WebElement fnameField = driver.findElement(By.xpath("//*[contains(@name, 'fname')]"));
+        String actualFNameValue = fnameField.getAttribute("value");
+        String expectedFNameValue = "First Name";
+        assertValue(actualFNameValue, expectedFNameValue);
+
+        WebElement lnameField = driver.findElement(By.xpath("//*[contains(@name, 'lname')]"));
+        String actualLNameValue = lnameField.getAttribute("value");
+        String expectedLNameValue = "Last Name";
+        assertValue(actualLNameValue, expectedLNameValue);
+
+        WebElement cityField = driver.findElement(By.xpath("//*[contains(@name, 'city')]"));
+        String actualCityValue = cityField.getAttribute("value");
+        String expectedCityValue = "LA";
+        assertValue(actualCityValue, expectedCityValue);
+
+        WebElement phoneField = driver.findElement(By.xpath("//*[contains(@name, 'phone')]"));
+        String actualPhoneValue = phoneField.getAttribute("value");
+        String expectedPhoneValue = "5553335353";
+        assertValue(actualPhoneValue, expectedPhoneValue);
+
+        /*driver.close();
+        driver.quit(); // Close Browser*/
 
     }
 
@@ -76,6 +112,15 @@ public class LoginTests {
         } else {
             System.err.println("Failed. Expected text is "
                     + expectedValue + ", but Actual title is " + actualValue);
+        }
+    }
+
+    public static void assertValue(String actualValue, String expectedValue) {
+        if (actualValue.equals(expectedValue)) {
+            System.out.println("Passed.");
+        } else {
+            System.err.println("Failed. Expected text is "
+                    + expectedValue + ", but Actual text is " + actualValue);
         }
     }
 
